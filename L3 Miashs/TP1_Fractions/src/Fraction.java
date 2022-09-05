a29c12cf08aff3a1074189a397b6b27579e504da
keserz <110199920+keserz@users.noreply.github.com>
2022-09-05 08:47:55 +0200

    Ajout du premier TP java

---
+++ L3 Miashs/TP1_Fractions/src/Fraction.java
@@ -0,0 +1,99 @@
+public class Fraction {
+    int n,d;
+    
+    
+    public Fraction() {
+        n = 0;
+        d = 1;
+    }
+    
+    public Fraction(int n) {
+        this.n = n;
+        d = 1;
+    }
+    
+    public Fraction(int n, int d) {
+        this.n = n;
+        this.d = d;    
+    }
+    
+    public Fraction(String ch) {
+        this.n = Integer.parseInt(ch.substring(0, ch.indexOf("/")));
+        this.d = Integer.parseInt(ch.substring(ch.indexOf("/")+1, ch.length()));
+    }
+    
+    public int getNumerateur() {
+        return n;
+    }
+    
+    public int getDenominateur() {
+        return d;
+    }
+    
+    public void setNumerateur(int n){
+        this.n=n;
+    }
+    
+    public void setDenominateur(int d) {
+        this.d=d;
+    }
+    
+    public String toString() {
+        String ch = this.n+"/"+this.d;
+        return ch;
+    }
+    
+    public static Fraction valueOf(String ch) {
+        Fraction f = new Fraction(ch);
+        return f;
+    }
+    
+    public int compareTo(Fraction f) {
+        return moins(f).n;
+        
+    }
+    
+    public boolean equals(Object obj) {
+        if (obj instanceof Fraction)  {
+            if (this.compareTo((Fraction)obj) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Fraction plus(Fraction f) {
+        int num = this.getNumerateur()*f.getDenominateur()+this.getDenominateur()*f.getNumerateur();
+        int den = this.getDenominateur()*f.getDenominateur();
+        
+//        System.out.println(pgcd(num,den));
+ 
+        return new Fraction(num/pgcd(num,den),den/pgcd(num,den));
+    }
+    
+    public Fraction moins(Fraction f) {
+        int num = this.getNumerateur()*f.getDenominateur()-this.getDenominateur()*f.getNumerateur();
+        int den = this.getDenominateur()*f.getDenominateur();
+        return new Fraction(num/pgcd(num,den),den/pgcd(num,den));
+    }
+    
+    public Fraction fois(Fraction f) {
+        int num = this.getNumerateur()*f.getNumerateur();
+        int den = this.getDenominateur()*f.getDenominateur();
+        return new Fraction(num/pgcd(num,den),den/pgcd(num,den));
+    }
+    
+    public Fraction divisePar(Fraction f) {
+    	int num = this.getNumerateur()*f.getDenominateur();
+    	int den = this.getDenominateur()*f.getNumerateur();
+    	return new Fraction(num/pgcd(num,den),den/pgcd(num,den));
+    }
+    
+    public int pgcd(int a, int b) {
+        if (a % b == 0)
+            return b;
+        else
+            return pgcd(b, a % b);
+    }
+    
+}
---
+++ L3 Miashs/TP1_Fractions/src/test.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class test {
+
+    
+    public static Fraction extractFraction(String entry) {
+    	int i=0;
+    	while (i< entry.length() && entry.charAt(i) != '+' && entry.charAt(i) != '-' && entry.charAt(i) != '*'&& entry.charAt(i)!= '=') {
+//			System.out.println(entry.charAt(i)+" allo "+ i);
+			i++;
+		}
+    	
+		Fraction f3 = new Fraction(entry.substring(0, i));
+//		System.out.println("test : "+f3);
+		return f3;
+    }
+    
+    public static String extractString(String entry) {
+    	int i=0;
+    	while (i< entry.length() && entry.charAt(i) != '+' && entry.charAt(i) != '-' && entry.charAt(i) != '*'&& entry.charAt(i)!= '=') {
+//			System.out.println(entry.charAt(i)+" allo "+ i);
+			i++;
+		}
+    	entry = entry.substring(i+1, entry.length());
+    	return entry;
+    }
+    
+//    public static char operation(String entry) {
+//    	int i=0;
+//    	while (i< entry.length() && entry.charAt(i) != '+' && entry.charAt(i) != '-' && entry.charAt(i) != '*'&& entry.charAt(i)!= '=') {
+////			System.out.println(entry.charAt(i)+" allo "+ i);
+//			i++;
+//		}
+//    	System.out.println("test char function : "+entry.charAt(i));
+//    	return entry.charAt(i);
+//    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ch = "123/53";
+		Scanner scanner = new Scanner(System.in);
+		int n = Integer.parseInt(ch.substring(0, ch.indexOf("/")));
+		int d = Integer.parseInt(ch.substring(ch.indexOf("/")+1, ch.length()));
+		Fraction f1 = new Fraction(0,1);
+		Fraction f2 = new Fraction(1,1);
+
+		System.out.println("Taper une expression terminée par =, la barre de fraction est /");
+		String entry = scanner.next();
+		System.out.print(entry);
+
+		f1 = extractFraction(entry);
+		String teste = entry;
+		int i=0;
+		while (extractString(teste).length() != 0) {
+	    	while (i< entry.length() && entry.charAt(i) != '+' && entry.charAt(i) != '-' && entry.charAt(i) != '*'&& entry.charAt(i)!= '=') {
+	//			System.out.println(entry.charAt(i)+" allo "+ i);
+				i++;
+			}
+
+	    	System.out.println("valeur de i :"+i);
+	    	System.out.println(entry.charAt(i));
+	    	teste = extractString(teste);
+	    	if (entry.charAt(i)=='+') {
+	    		System.out.println("test de + f1 :"+f1);
+	    		f1=f1.plus(extractFraction(teste));
+	    		System.out.println("test de + f1 :"+f1);
+	    	}
+			if (entry.charAt(i)=='-') {
+				System.out.println("test de - f1 :"+f1);
+				f1 = f1.moins(extractFraction(teste));
+				System.out.println("test de - f1 :"+f1);
+			}
+		
+			if (entry.charAt(i)=='*') {
+				System.out.println("test * de f1 :"+f1);
+				f1 = f1.fois(extractFraction(teste));
+				System.out.println("test * de f1 :"+f1);
+			}
+			i++;
+		
+		}
+		
+		
+		
+//		while (extractString(teste).length() != 0) {
+//			
+//			teste = extractString(teste);
+//			System.out.println("print ln de teste : "+teste);
+//			if (operation(teste)=='+') {
+//				System.out.println("test de f1 :"+f1);
+//				f1 = f1.plus(extractFraction(teste));
+//			}
+////			
+////			if (operation(teste)=='-') {
+////				f1 = f1.moins(extractFraction(teste));
+////			}
+////			
+////			if (operation(teste)=='*') {
+////				f1 = f1.fois(extractFraction(teste));
+////			}
+//			
+////			f1 = f1.plus(extractFraction(teste));
+//
+//			
+//		}
+//		
+		System.out.print(f1);
+		
+		
+		
+	}
+
+}
