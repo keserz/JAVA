b383b9cbdbcd5ba1cebe26858064ab991791a6bf
keserz <Fouad.Gasmi@etu.univ-grenoble-alpes.fr>
2022-09-11 08:00:14 +0200

    ??

--- TD1_String/src/String_StringBuffer/String.java
+++ TD1_String/src/String_StringBuffer/String.java
@@ -115,6 +115,7 @@ public class String extends Object{
 	public static String valueOf(int i) {
 //		Retourne la représentation en chaîne de l’entier i.
 		return new String();
+		//bonjour
 	}
 	
 	
